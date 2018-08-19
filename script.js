f953e725dd30d96cd09be12ba067bddd028f48a8
Andualem Kelbessa <andualem.u.kelbessa.19@dartmouth.edu>
2018-08-19 19:49:56 -0400

    complete site (except content)

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
+$(document).ready(function() {
 
+$(function(){
+    $('.type').typed({
+        strings: [
+            "a Coder.",
+            "an Engineer.",
+            "a Graphic Designer.",
+            "a Runner.",
+            "a Soccer Player."
+        ],
+        typeSpeed: 100,
+        backDelay: 300,
+        loop: true,
+    })
+});
+
+$('.fullBackground').fullClip({
+    images: [ "./images/baker.jpg",
+             "./images/bench.jpg",
+            "./images/darthall.jpg",
+            "./images/frat.jpg"],
+    transitionTime: 2000,
+    wait: 5000
+})
+    
+
+});
 
