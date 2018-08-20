ee74a919bb8799758e3a4fdb1119e4ebb4877b3c
Andualem Kelbessa <andualem.u.kelbessa.19@dartmouth.edu>
2018-08-19 21:59:28 -0400

    full website except description of projects

--- script.js
+++ script.js
@@ -16,14 +16,11 @@ $(function(){
 });
 
 $('.fullBackground').fullClip({
-    images: [ "./images/baker.jpg",
-             "./images/bench.jpg",
-            "./images/darthall.jpg",
-            "./images/frat.jpg"],
+    images: [ "https://lh3.googleusercontent.com/kFWPTYkFGCkq_Ib--9EE3twYhzphU53JfecG01x0wwd8G83E6JT5RBjXFEpIrtECIYeyDHQlvmAFUqokdg68wcFnPbEA2fRSXy5TJYPBQnGckqQpspTXt942RJMGjToVFiTS2wN0LQ=w2400", "https://lh3.googleusercontent.com/PGHjjUggovjRpCQSEFyfPYbmvsSNIfZf-l9frMwd-zshYo6U9FLazOFuyd6-8-C9m65e5hIxibhOgS4IyPXqGsLTQ7bvOX8F0E4lsn_26r_vhJg0x4xWZZ174swGLEPNexMc5CM0vw=w2400",
+    "https://lh3.googleusercontent.com/N--8LPUm2Ss780OXyZRD_-RTZnd1CJi57qndyjVEzTqaGAri3SN1uOzr4kAHzTH3S1QdSui1XSg0kU6o1Wp-S5lzixs-_RSP1mg0_u98lr0ou02-YV6ToIB96OEvhzEQxdypTQhQvw=w2400", "https://lh3.googleusercontent.com/67sFWRfh9XHDVem9P47W3SOgaOg1jR2fm3Fh31X3vi8FZontPUVuRwPQyHCDD8NtqpkPkh7mrjinf_cvAuDX8oBLKebkXgyyI3fH3ygUt-2pPWksEYpqoqxbkcMw39z9X_tvdvwfSA=w2400"],
     transitionTime: 2000,
     wait: 5000
 })
-    
 
 });
 
